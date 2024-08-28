99bd48901abce2257f37ee806fe419544369114f
jiaayy <1020538349@qq.com>
2024-08-29 00:04:14 +0800

    测试

---
+++ JIAY/JIAY/JIAY.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+	int age = 16;
+	double weight = 100.5;
+	age = age + 1;
+	weight = weight + 5;
+	printf("JIAY/n");
+	printf("%d/n", age);
+	printf("%lf/n", weight);
+	"return0";
+}
